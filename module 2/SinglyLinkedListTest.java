54b242c06a4ad58f6ed24f71da40d0ea9df33008
Vince Tran <vietanhtran.96@gmail.com>
2022-04-15 12:57:56 -0500

    add addAtIndex for SinglyLinkedList

--- module 2/SinglyLinkedListTest.java
+++ module 2/SinglyLinkedListTest.java
@@ -1,22 +1,31 @@
 public class SinglyLinkedListTest<T> {
     // Instance vars
     private Node<T> head;
+    private int size;
 
     // Constructor
     public SinglyLinkedListTest() {
         this.head = null;
+        this.size = 0;
+    }
+
+    // Getters, setters
+    public int getSize() {
+        return this.size;
     }
 
     // Add methods
     public void addToFront(T data) {
-        // Create new node; Change its next; Change this.head
+        // Create new node; change its next;
+        // change this.head; increase size
         Node<T> newNode = new Node<T>(data);
         newNode.next = this.head;
         this.head = newNode;
+        this.size++;
     }
 
     public void addToBack(T data) {
-        // Create new node; consider 2 cases
+        // Create new node; consider 2 cases; increase size
         Node<T> newNode = new Node<T>(data);
 
         if (this.head == null) {
@@ -24,13 +33,45 @@ public class SinglyLinkedListTest<T> {
         }
         else {
             Node<T> currentNode = this.head;
-
             // Traverse thru the list
             while (currentNode.getNext() != null) {
                 currentNode = currentNode.getNext();
             }
             currentNode.setNext(newNode);
         }
+        this.size++;
+    }
+
+    public void addAtIndex(T data, int index) {
+        // Check illegal input cases
+        if (data == null) {
+            throw new IllegalArgumentException("Null data cannot be added");
+        }
+        else if (index < 0 || index > this.getSize()) {
+            throw new IllegalArgumentException("Index is out of bounds");
+        }
+
+        // Check if list empty; traverse thru to add
+        Node<T> newNode = new Node<T>(data);
+        
+        if (this.head == null) {
+            this.head = newNode;
+        }
+        else {
+            Node<T> currentNode = this.head;
+            // Special case if index == 0
+            if (index == 0) {
+                newNode.setNext(currentNode);
+            }
+            else{
+                for (int i = 1; i < index; i++) {
+                    currentNode = currentNode.getNext();
+                }
+                newNode.setNext(currentNode.getNext());
+                currentNode.setNext(newNode);
+            }
+        }
+        this.size++;
     }
 
     // Remove methods
@@ -48,7 +89,7 @@ public class SinglyLinkedListTest<T> {
      */
     public void removeFromBack() {
         if (this.head != null) {
-            if (this.head.next == null) {
+            if (this.head.getNext() == null) {
                 this.head = null;
             }
             else {
@@ -103,6 +144,13 @@ public class SinglyLinkedListTest<T> {
         // To string method
         public String toString() {
             return this.data.toString();
+        }
+    }
+
+    public static void main(String[] args) {
+        int l = 1;
+        for (int i = 1; i < l; i++) {
+            System.out.println("???");
         }
     }
 }
