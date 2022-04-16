495e8f4c776599c9f8f4ef19465f5c6d6f7dbfff
Vince Tran <vietanhtran.96@gmail.com>
2022-04-15 21:20:51 -0500

    for SinglyLinkedList: add removeAtIndex; add check for illegal input for some add and remove methods; add some test cases

--- module 2/SinglyLinkedListTest.java
+++ module 2/SinglyLinkedListTest.java
@@ -16,6 +16,11 @@ public class SinglyLinkedListTest<T> {
 
     // Add methods
     public void addToFront(T data) {
+        // Check illegal input cases
+        if (data == null) {
+            throw new IllegalArgumentException("Null data cannot be added");
+        }
+        
         // Create new node; change its next;
         // change this.head; increase size
         Node<T> newNode = new Node<T>(data);
@@ -25,6 +30,11 @@ public class SinglyLinkedListTest<T> {
     }
 
     public void addToBack(T data) {
+        // Check illegal input cases
+        if (data == null) {
+            throw new IllegalArgumentException("Null data cannot be added");
+        }
+        
         // Create new node; consider 2 cases; increase size
         Node<T> newNode = new Node<T>(data);
 
@@ -42,6 +52,9 @@ public class SinglyLinkedListTest<T> {
         this.size++;
     }
 
+    /**
+     * Check for illgal input first
+     */
     public void addAtIndex(T data, int index) {
         // Check illegal input cases
         if (data == null) {
@@ -51,69 +64,95 @@ public class SinglyLinkedListTest<T> {
             throw new IllegalArgumentException("Index is out of bounds");
         }
 
-        // Check if list empty; traverse thru to add
+        // Traverse thru list to add
         Node<T> newNode = new Node<T>(data);
-        
-        if (this.head == null) {
+        // Special case if index == 0
+        if (index == 0) {
+            newNode.setNext(this.head);
             this.head = newNode;
         }
-        else {
+        else{
             Node<T> currentNode = this.head;
-            // Special case if index == 0
-            if (index == 0) {
-                newNode.setNext(currentNode);
+            for (int i = 1; i < index; i++) {
+                currentNode = currentNode.getNext();
             }
-            else{
-                for (int i = 1; i < index; i++) {
-                    currentNode = currentNode.getNext();
-                }
-                newNode.setNext(currentNode.getNext());
-                currentNode.setNext(newNode);
-            }
+            newNode.setNext(currentNode.getNext());
+            currentNode.setNext(newNode);
         }
         this.size++;
     }
 
     // Remove methods
     /**
-     * Checks if head is null first
+     * Check if head is null first
      */
     public void removeFromFront() {
-        if (this.head != null) {
-            this.head = this.head.getNext();
+        if (this.head == null) {
+            throw new ArrayIndexOutOfBoundsException("Cannot remove data from empty list");
         }
+        
+        this.head = this.head.getNext();
+        this.size--;
     }
 
     /**
      * Only traverse to the *penultimate* element
      */
     public void removeFromBack() {
-        if (this.head != null) {
-            if (this.head.getNext() == null) {
-                this.head = null;
-            }
-            else {
-                Node<T> currentNode = this.head;
-                while (currentNode.getNext().getNext() != null) {
-                    currentNode = currentNode.getNext();
-                }
-                currentNode.setNext(null);
-            }
+        if (this.head == null) {
+            throw new ArrayIndexOutOfBoundsException("Cannot remove data from empty list");
         }
+        
+        if (this.head.getNext() == null) {
+            this.head = null;
+        }
+        else {
+            Node<T> currentNode = this.head;
+            while (currentNode.getNext().getNext() != null) {
+                currentNode = currentNode.getNext();
+            }
+            currentNode.setNext(null);
+        }
+        this.size--;
     }
 
-    // To string method
+    /**
+     * Check for illegal input first
+     */
+    public void removeAtIndex(int index) {
+        // Check illegal input cases
+        if (index < 0 || index > this.getSize()) {
+            throw new IllegalArgumentException("Index is out of bounds");
+        }
+        else if (this.head == null) {
+            throw new ArrayIndexOutOfBoundsException("Cannot remove data from empty list");
+        }
+
+        if (index == 0) {
+            this.head = this.head.getNext();
+        }
+        else {
+            Node<T> currentNode = this.head;
+            for (int i = 1; i < index; i++) {
+                currentNode = currentNode.getNext();
+            }
+            currentNode.setNext(currentNode.getNext().getNext());
+        }
+        this.size--;
+    }
+
+    // To string method, use comma as a separator
     public String toString() {
-        String return_string = "";
+        String listAsString = "";
         Node<T> currentNode = this.head;
 
         // Traverse thru the list
         while (currentNode != null) {
-            return_string += currentNode.toString() + " ";
+            listAsString += currentNode.toString() + ", ";
             currentNode = currentNode.getNext();
         }
 
-        return return_string;
+        return listAsString;
     }
     
     /**
@@ -148,9 +187,117 @@ public class SinglyLinkedListTest<T> {
     }
 
     public static void main(String[] args) {
-        int l = 1;
-        for (int i = 1; i < l; i++) {
-            System.out.println("???");
+        // Add to front
+        System.out.println("Add to front:");
+        SinglyLinkedListTest<Integer> a0 = new SinglyLinkedListTest<>();
+        a0.addToFront(1);
+        a0.addToFront(2);
+        a0.addToFront(3);
+        System.out.println("a0: " + a0.toString());
+        System.out.println("Add to front with null data, catch exception:");
+        try {
+            a0.addToFront(null);
         }
+        catch (IllegalArgumentException exc) {
+            System.out.println("Caught illegal arg exception");
+        }
+
+        // Add to back
+        System.out.println("Add to back:");
+        SinglyLinkedListTest<Integer> a1 = new SinglyLinkedListTest<>();
+        a1.addToBack(-1);
+        a1.addToBack(-2);
+        a1.addToBack(-3);
+        System.out.println("a1: " + a1.toString());
+        System.out.println("Add to back with null data, catch exception:");
+        try {
+            a1.addToBack(null);
+        }
+        catch (IllegalArgumentException exc) {
+            System.out.println("Caught illegal arg exception");
+        }
+
+        // Remove from front
+        // Empty and regular list
+        System.out.println("Remove from front for empty list, catch exception:");
+        SinglyLinkedListTest<Integer> r0 = new SinglyLinkedListTest<>();
+        try {
+            r0.removeFromFront();
+        }
+        catch (ArrayIndexOutOfBoundsException exc) {
+            System.out.println("Caught index OOB exception");
+        }
+        System.out.println("r0: " + r0.toString());
+        System.out.println(r0.getSize());
+
+        System.out.println("Remove from front for regular list:");
+        a0.removeFromFront();
+        System.out.println("a0: " + a0.toString());
+        System.out.println(a0.getSize());
+
+        // Remove from back
+        // Empty and regular list
+        System.out.println("Remove from back for empty list, catch exception:");
+        SinglyLinkedListTest<Integer> r1 = new SinglyLinkedListTest<>();
+        try {
+            r1.removeFromBack();
+        }
+        catch (ArrayIndexOutOfBoundsException exc) {
+            System.out.println("Caught index OOB exception");
+        }
+        System.out.println("r1: " + r1.toString());
+        System.out.println(r1.getSize());
+
+        System.out.println("Remove from back for regular list, catch exception:");
+        a1.removeFromBack();
+        System.out.println("a1: " + a1.toString());
+        System.out.println(a1.getSize());
+
+        // Add at index
+        // Empty and regular list
+        System.out.println("Add at index, catch exception:");
+        try {
+            r0.addAtIndex(1, 1);
+        }
+        catch (IllegalArgumentException exc) {
+            System.out.println("Caught illegal arg exception");
+        }
+        System.out.println("Add at index with null data, catch exception:");
+        try {
+            r0.addAtIndex(null, 0);
+        }
+        catch (IllegalArgumentException exc) {
+            System.out.println("Caught illegal arg exception");
+        }
+        System.out.println("Add at index for empty list:");
+        r0.addAtIndex(1, 0);
+        System.out.println("r0: " + r0.toString());
+        System.out.println(r0.getSize());
+
+        System.out.println("Add at index for regular list:");
+        a1.addAtIndex(-4, 1);
+        System.out.println("a1: " + a1.toString());
+        System.out.println(a1.getSize());
+
+        // Remove at index
+        // Empty list and regular list
+        System.out.println("Remove at index, catch exception:");
+        try {
+            r1.removeAtIndex(3);
+        }
+        catch (IllegalArgumentException exc) {
+            System.out.println("Caught illegal arg exception");
+        }
+        try {
+            r1.removeAtIndex(0);
+        }
+        catch (ArrayIndexOutOfBoundsException exc) {
+            System.out.println("Caught index OOB exception");
+        }
+
+        System.out.println("Remove at index for regular list:");
+        a0.removeAtIndex(1);
+        System.out.println("a0: " + a0.toString());
+        System.out.println(a0.getSize());
     }
 }
